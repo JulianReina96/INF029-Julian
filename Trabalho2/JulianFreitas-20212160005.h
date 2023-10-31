005ccc8cc1c84bae1bcff25a8762f7f06566cf3f
JulianReina96 <87534209+JulianReina96@users.noreply.github.com>
2023-10-31 22:51:36 +0000

    --Inclusão Trabalho 2

---
+++ Trabalho2/JulianFreitas-20212160005.h
@@ -0,0 +1,62 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  Cada aluno deve renomear esse arquivo para Aluno<MATRICULA>.c
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do
+//  trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome: Julian Freitas Reina
+//  email: julianreina@gmail.com
+//  Matrícula: 20212160005
+//  Semestre: 2
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 07/05/2021
+
+// #################################################
+
+
+#ifndef JulianFreitas_20212160005
+#define JulianFreitas_20212160005
+
+// enumeracoes (enum) ajudam a deixar o codigo mais legivel, possibilitando que voce de significado
+// as suas constantes, para mais informacoes https://en.cppreference.com/w/c/language/enum
+// as constantes do enum em sua inicialização vao representar o numero resultante da soma do valor da
+// constante anterior mais 1, caso nao haja valor algum na primeira constante ela vai ser inicializada com 0
+enum { TODAS_ESTRUTURAS_AUXILIARES_VAZIAS = -11, NOVO_TAMANHO_INVALIDO, NUMERO_INEXISTENTE,
+       ESTRUTURA_AUXILIAR_VAZIA, TAMANHO_INVALIDO, SEM_ESPACO_DE_MEMORIA, POSICAO_INVALIDA,
+       JA_TEM_ESTRUTURA_AUXILIAR, SEM_ESTRUTURA_AUXILIAR, SEM_ESPACO, SUCESSO };
+
+typedef struct reg {
+  int conteudo;
+  struct reg *prox;
+} No;
+
+
+
+int criarEstruturaAuxiliar(int posicao, int tamanho);
+int inserirNumeroEmEstrutura(int posicao, int valor);
+int excluirNumeroDoFinaldaEstrutura(int posicao);
+int excluirNumeroEspecificoDeEstrutura(int posicao, int valor);
+int getDadosEstruturaAuxiliar(int posicao, int vetorAux[]);
+int getDadosOrdenadosEstruturaAuxiliar(int posicao, int vetorAux[]);
+int getDadosDeTodasEstruturasAuxiliares(int vetorAux[]);
+int getDadosOrdenadosDeTodasEstruturasAuxiliares(int vetorAux[]);
+int modificarTamanhoEstruturaAuxiliar(int posicao, int novoTamanho);
+int getQuantidadeElementosEstruturaAuxiliar(int posicao);
+No *montarListaEncadeadaComCabecote();
+void getDadosListaEncadeadaComCabecote(No *inicio, int vetorAux[]);
+void destruirListaEncadeadaComCabecote(No **inicio);
+
+void inicializar();
+void finalizar();
+void dobrar(int *x);
+
+#endif  // TRABALHO2_ESTRUTURAVETORES_H
